90b2ddaaeb831315ba6b3514ccf237b53acd097a
Daniel Müller <miladaniel370@gmail.com>
2025-11-25 20:49:52 -0300

    DM-2 feat: Adicionando metodo de resistenciaAoSopro

--- src/com/aula/Casa.java
+++ src/com/aula/Casa.java
@@ -9,11 +9,13 @@ package com.aula;
 public class Casa {
 
     private String material;
-    private boolean forcaDoSopro;
+    private boolean resisteAoSopro;
+    private int forcaDoSopro;
     private String statusAtual;
 
-    public Casa(String material, boolean forcaDoSopro, String statusAtual) {
+    public Casa(String material, boolean resisteAoSopro, int forcaDoSopro, String statusAtual) {
         this.material = material;
+        this.resisteAoSopro = resisteAoSopro;
         this.forcaDoSopro = forcaDoSopro;
         this.statusAtual = statusAtual;
     }
@@ -23,5 +25,13 @@ public class Casa {
      * */
     public String getMaterial() {
         return material;
+    }
+
+    /**
+     * @param forcaDoSopro mostra a força do sopro do lobo.
+     * @return resistencia do sopro vai mostrar se a casa é resistente ao sopro mostrando TRUE como resistente e FALSE como não resistente.
+     * */
+    public boolean isResisteAoSopro(int forcaDoSopro) {
+        return resisteAoSopro;
     }
 }
